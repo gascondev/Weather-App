c2a5b2b07bd0e02cdfb948aa8c478dbc9882766a
Álvaro Gascón <alvarogascon@hotmail.com>
2024-03-05 07:09:24 +0100

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Álvaro Gascón on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
